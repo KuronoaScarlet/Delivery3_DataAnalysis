f712230f49adfe33118f6e420f2fe0f62289b81c
Makinilla-maker <valverdeoriol@gmail.com>
2023-01-02 19:49:10 +0100

    Data saver done

--- Delivery3_DataAnalysis/Assets/HitManager.cs
+++ Delivery3_DataAnalysis/Assets/HitManager.cs
@@ -12,17 +12,20 @@ public enum HitType
     LAVA,
     NONE,
 }
+[System.Serializable]
 public class HitData
 {
     public HitType hitType;
+    public string damager;
     public Vector3 postion;
     public Vector3 forward;
     public float time;
 
     public HitData() { }
-    public HitData(HitType h, Vector3 pos, Vector3 fw, float t)
+    public HitData(HitType h, string d, Vector3 pos, Vector3 fw, float t)
     {
         hitType = h;
+        damager = d;
         postion = pos;
         forward = fw;
         time = t;
@@ -34,7 +37,7 @@ public class HitManager : MonoBehaviour, IMessageReceiver
     Damageable m_Damageable;
     private void OnEnable()
     {
-        m_Damageable = GameObject.FindObjectOfType<Damageable>();
+        m_Damageable = GameObject.Find("Ellen").GetComponent<Damageable>();
         m_Damageable.onDamageMessageReceivers.Add(this);
 
     }
@@ -42,25 +45,15 @@ public class HitManager : MonoBehaviour, IMessageReceiver
     {
         m_Damageable.onDamageMessageReceivers.Remove(this);
     }
-
-    public void AddHitNonLethalPlayer(float a, GameObject player)
-    {
-        hits.Add(new HitData(HitType.NONLETHAL, player.transform.position, player.transform.forward, Time.time));
-    }
-    public void AddHitNonLethalPlayer(HitDataMono mono)
-    {
-        hits.Add(mono.data);
-    }
-
     public void OnReceiveMessage(MessageType type, object sender, object msg)
     {
         switch (type)
         {
             case MessageType.DAMAGED:
-                hits.Add(new HitData(HitType.NONLETHAL, ((MonoBehaviour)sender).transform.position, ((DamageMessage)msg).direction, Time.time));
+                hits.Add(new HitData(HitType.NONLETHAL, ((DamageMessage)msg).damager.gameObject.transform.root.name ,((MonoBehaviour)sender).transform.position, ((DamageMessage)msg).direction, Time.time));
                 break;
             case MessageType.DEAD:
-                hits.Add(new HitData(HitType.LETHAL, ((MonoBehaviour)sender).transform.position, ((DamageMessage)msg).direction, Time.time));
+                hits.Add(new HitData(HitType.LETHAL, ((DamageMessage)msg).damager.gameObject.transform.root.name, ((MonoBehaviour)sender).transform.position, ((DamageMessage)msg).direction, Time.time));
                 break;
             default:
                 break;
